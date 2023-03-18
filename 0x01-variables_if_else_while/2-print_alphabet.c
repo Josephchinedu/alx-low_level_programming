5e4d8500acb1e270e027f04388d8ee8deace5773
Josephchinedu <josephchinedu34@gmail.com>
2023-03-18 03:04:58 -0700

    2-print_alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,9 +6,9 @@ int main(void)
 {
     char low;
 
-    for (low = 'a'; low <= 'z'; low++) {
+    for (low = 'a'; low <= 'z'; low++) 
         putchar(low);
-    }
+    
     putchar('\n');
 
     return 0;
